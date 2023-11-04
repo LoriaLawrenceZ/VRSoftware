8f69ae9bc8f3b41280db48f5ca681e401d7f799c
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-11-04 09:33:28 -0300

    Calculadora

--- Java/ApacheNetbeans/Aula1/src/aula1/Aula1.java
+++ Java/ApacheNetbeans/Aula1/src/aula1/Aula1.java
@@ -12,7 +12,7 @@ public class Aula1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
         Calculadora calculadora = new Calculadora(sc);
         calculadora.menu();
